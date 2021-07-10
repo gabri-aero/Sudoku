e6b5bd9fa905f3db1d356c9b937921c1df464b97
Gabriel <gvallesv@outlook.es>
2021-07-10 20:39:32 +0200

    commit on 'master'

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,21 +1,7 @@
 import numpy as np
 
-board = [
-    [0,4,0,9,0,0,6,0,1],
-    [7,8,0,0,0,0,0,3,0],
-    [1,0,5,0,0,4,9,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,9,0,5,4,0,2,0,6],
-    [0,0,0,0,0,1,0,0,7],
-    [0,7,0,0,8,9,1,0,0],
-    [0,0,0,1,2,0,0,0,4],
-    [8,0,0,0,0,0,0,9,0]
-]
-
-memory=list(map(lambda x: list(map(lambda y: True if y==0 else False,x)), board))
-
 def printSudoku(sudoku): #board design in console
-    result=sudoku.tolist()
+    result=sudoku
     for i in range(len(result)):
         result[i].insert(9,'|')
         result[i].insert(6,'|')
@@ -28,28 +14,27 @@ def printSudoku(sudoku): #board design in console
             print(jtem, end=' ')
         print('\n', end='')
 
-board=np.array(board) #enabling numpy functionalities in the board matrix
-
-def check(num,posX,posY, sudoku):
+def check(num, posX, posY, sudoku):
+    sudoku=np.array(sudoku) #enabling numpy functionalities in the matrix
     if(num in sudoku[posX]): return False #row
-    elif(num in sudoku[:,posY]): return False #column
+    elif(num in sudoku[:, posY]): return False #column
     elif(num in sudoku[3*(posX//3):3*(posX//3)+3, 3*(posY//3):3*(posY//3)+3]): return False #submatrix 3x3
     else: return True
 
-def solve(sudoku):
+def solve(board):
+    sudoku = list(map(lambda x: list(map(lambda y: y, x)), board)) #avoiding array mutability
+    memory=list(map(lambda x: list(map(lambda y: True if y==0 else False,x)), sudoku))
     i=j=0
     while i<len(sudoku):
         while j<len(sudoku):
             if memory[i][j]:
-                i, j= writeNum(i,j,sudoku) #resetting indexes
+                i, j= writeNum(i,j,sudoku, memory) #resetting indexes
             j+=1
         i+=1
         j=0
-    print('\nSolution: ')
-    printSudoku(sudoku)
-    pass
+    return sudoku
 
-def writeNum(i,j,sudoku):
+def writeNum(i, j, sudoku, memory):
     num=sudoku[i][j]
     while(num<9):
         num+=1
@@ -66,6 +51,3 @@ def writeNum(i,j,sudoku):
         if memory[i][j]:
             return i, j-1 #backtracking to position i,j
 
-print('Board to solve: ')
-printSudoku(board)
-solve(board)
